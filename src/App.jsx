9008582c27b2d9a7c723ae2702aadbb53fc20ae0
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-02-19 19:39:00 -0300

    Adiciona cores e fontes nas variáveis css
    - Adição das cores nas variáveis css.
    - Importação no index.html e adição das fontes do projeto.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-black font-bold text-white p-10'>
+    <div className='bg-(--primary-color) font-bold text-white p-10'>
       <p>
         Hello, World!
       </p>
